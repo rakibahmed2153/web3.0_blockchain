9a02a3e15d9d48d113488aef88f237d83f70608f
Rakib Ahmed <36979611+rakibahmed2153@users.noreply.github.com>
2022-09-24 14:25:56 +0600

    Navbar and welcome
    Navbar and welcome page design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,15 @@
-import { Navbar, Welcome, Footer, Services, Transations } from "./components"
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
-const App = () => {
-  return (
-    <div className="min-h-screen">
-        <div className="gradient-bg-welcome">
-            <Navbar/>
-            <Welcome/>
-        </div>
-        <Services/>
-        <Transations/>
-        <Footer/>
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
-  )
-}
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
